f438f11b8ae0a9b09076a93a0d9c3886ca3eb324
j.michel <j.michel@wavenet.be>
2025-11-26 18:36:45 +0100

    Commit test class

--- src/models/Test.java
+++ src/models/Test.java
@@ -1,4 +1,18 @@
 package models;
 
 public class Test {
+
+  private int test;
+
+  public Test(int test) {
+    this.test = test;
+  }
+
+  public int getTest() {
+    return test;
+  }
+
+  public void setTest(int test) {
+    this.test = test;
+  }
 }
