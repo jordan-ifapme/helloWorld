3f3b368237787051fe70cbf2f7e751dc6465b6e5
Amelie Petre <ameliepetre05@gmail.com>
2025-11-26 19:24:18 +0100

    Commit Amelie Petre

--- src/models/Product.java
+++ src/models/Product.java
@@ -1,17 +1,22 @@
 package models;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Product {
     private String name;
     private double price;
     private boolean inStock;
     private int discountPercentage;
     public static int globalStockQuantity = 0;
+    public LocalDateTime DateDeCreation;
 
     public Product(String name, double price, boolean inStock, int discountPercentage) {
         this.name = name;
         this.price = price;
         this.inStock = inStock;
         this.discountPercentage = discountPercentage;
+        this.DateDeCreation = LocalDateTime.now();
         if (inStock) Product.globalStockQuantity ++;
     }
 
